26e8923ef11d47515acb79445c43946471e47bbf
zy <123456789@qq.com>
2026-04-02 19:36:32 +0800

    work5添加拦截器

---
+++ work4/demo/src/main/java/com/xzy212306259/demo/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.xzy212306259.demo.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.xzy212306259.demo.Interceptor.AuthInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .addPathPatterns("/api/**")
+                .excludePathPatterns(
+                        "/api/users/login"
+                );
+    }
+}
